0243a870d2f049c15c09dfc4951ecd0ca331a5a4
Ariel Dante <ariel@cookunity.com>
2024-03-13 17:25:04 -0300

    feat: improve home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-black">
+      <Image src="/logo.png" width="272" height="272" alt="Chef Pulse" />
       <div className="h-2">
-        <h2 className="mb-8 text-6xl font-bold leading-tight text-center">
-          Welcome to Chef Pulse!
-        </h2>
-        <p className="mb-8 text-2xl text-center">Please, give us feedback on...</p>
+        <p className="mb-8 text-2xl text-center text-white">Please, give us feedback on...</p>
       </div>
 
       <div className="mt-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left">
         <Link
           href="/pulse"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="text-white hover:text-black group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Ingredients{" "}
@@ -29,7 +27,7 @@ export default function Home() {
 
         <a
           href="#"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 disabled:opacity-50"
+          className="text-white hover:text-black group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 disabled:opacity-50"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
@@ -45,7 +43,7 @@ export default function Home() {
 
         <a
           href="#"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          className="text-white hover:text-black group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
