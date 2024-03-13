813ed2c212610b58ad08090559f1ad8f5c0c773a
Juan Manuel Agüero <juanmanuelaguero@ip-172-17-17-111.ec2.internal>
2024-03-13 16:12:58 -0300

    feat: init

---
+++ src/app/pulse/fakeData.ts
@@ -0,0 +1,23 @@
+export type IngredienteType = {
+  name: string;
+  url: string;
+};
+
+export const ingredients: IngredienteType[] = [
+  {
+    name: "Tomatoe",
+    url: "./img/tomate_01.jpg",
+  },
+  {
+    name: "Lettuce",
+    url: "./img/lechuga_01.jpg",
+  },
+  {
+    name: "Salmon",
+    url: "./img/salmon_01.jpeg",
+  },
+  {
+    name: "Salt",
+    url: "./img/sal_01.jpg",
+  },
+];
